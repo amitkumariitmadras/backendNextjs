a74360d5a87b91afe72a3f4e3861b555292d786a
amitkumariitm <amitkumarsumit62027@gmail.com>
2024-10-05 02:56:21 +0530

    added delete request in dynamic category

--- app/api/(dashboard)/category/[category]/route.ts
+++ app/api/(dashboard)/category/[category]/route.ts
@@ -59,7 +59,57 @@ export const PATCH = async(request: Request, context:{params: any})=>{
     } catch(error: any){
         return new NextResponse(JSON.stringify({message: error.message}), {status:500});
     }
+}
 
+export const DELETE = async (request: Request, context:{params: any})=>{
 
+    try{
+        
+        const categoryId = context.params.category;
+
+        const {searchParams} = new URL(request.url);
+        const userId = searchParams.get("userId");
+        
+        if(!userId || !Types.ObjectId.isValid(userId)){
+            return new NextResponse(JSON.stringify({message: "Invalid or missing userId"}), {status:400});
+        }
+
+        if(!categoryId || !Types.ObjectId.isValid(categoryId)){
+            return new NextResponse(JSON.stringify({message: "invalid or Missing categoryId"}), {status: 400});
+        }
+
+        await connect();
+
+        const user = await User.findById(userId);
+        console.log(user);
+        
+        if(!user){
+            return new NextResponse(JSON.stringify({message: "User not found"}),{status: 404});
+        }
+
+        const category = await Category.findOne(
+           { _id: categoryId,
+            user: userId
+        }
+        )
+
+        // console.log("category", category);
+
+        if(!category){
+            return new NextResponse(JSON.stringify({message: "Category not found"}), {status: 404});
+        }
+
+        const delCategory = await Category.findByIdAndDelete(
+            categoryId
+        )
+
+        console.log(delCategory);
+
+        return new NextResponse(JSON.stringify({message: "category deleted", category: delCategory}),{status: 200})
+        
+
+    }catch(error: any){
+        return new NextResponse(JSON.stringify({message: error.message}), {status:400});
+    }
 
 }
