5f4afe8a8a4a8f29ac5cb99051d5dfd41b100fbf
amitkumariitm <amitkumarsumit62027@gmail.com>
2024-10-05 05:36:06 +0530

    added filtering in the get

--- app/api/(dashboard)/blogs/route.ts
+++ app/api/(dashboard)/blogs/route.ts
@@ -15,6 +15,12 @@ export const GET = async(request: Request) =>{
     const userId = searchParams.get("userId");
     const categoryId = searchParams.get("categoryId");
 
+    const searchKeywords = searchParams.get("keywords") as string;
+    const startDate = searchParams.get("startDate");
+    const endDate = searchParams.get("endDate");
+    const page: any = parseInt(searchParams.get("page") || "1");
+    const limit: any = parseInt(searchParams.get("limit") || "10");
+
     console.log(categoryId);
 
     if (!userId || !Types.ObjectId.isValid(userId)) {
@@ -49,10 +55,40 @@ export const GET = async(request: Request) =>{
           });
       }
 
-      const newBlog = await Blog.find({
+      const filter: any = {
         user: new Types.ObjectId(userId),
         category: new Types.ObjectId(categoryId),
-      })
+      };
+  
+      if (searchKeywords) {
+        filter.$or = [
+          {
+            title: { $regex: searchKeywords, $options: "i" },
+          },
+          {
+            description: { $regex: searchKeywords, $options: "i" },
+          },
+        ];
+      }
+  
+      if (startDate && endDate) {
+        filter.createdAt = {
+          $gte: new Date(startDate),
+          $lte: new Date(endDate),
+        };
+      } else if (startDate) {
+        filter.createdAt = {
+          $gte: new Date(startDate),
+        };
+      } else if (endDate) {
+        filter.createdAt = {
+          $lte: new Date(endDate),
+        };
+      }
+  
+      const skip = (page - 1) * limit;
+
+      const newBlog = await Blog.find(filter).sort({createdat:"asc"}).skip(skip).limit(limit);
 
  
       return new NextResponse(JSON.stringify({newBlog}), {status: 200});
